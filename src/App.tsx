54a347e66cf9a13b7c4911d68b3deccbd8617617
gkotecki <gabrielkotecki@gmail.com>
2022-09-28 17:50:44 -0300

    UPDATE: folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Button } from './components/shared/Button';
+import { Button } from './components/Button';
 import './index.css';
+import { Home } from './views/Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1> Color Interpolator </h1>
-    <Button>button</Button>
+    <Home/>
   </React.StrictMode>,
 );
---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import { Button } from '../components/Button';
+
+/**
+ * Home page component
+ */
+export const Home = () => {
+  return (
+    <>
+      <h1> Color Interpolator </h1>
+      <Button>button</Button>
+    </>
+  );
+};
